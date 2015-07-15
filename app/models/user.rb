b703bf4b1a94b4fcb3db9530b957da5b20596e73
Evangeline Garreau & Shawn Wilkinson <pair+egarreau.shawndav@devbootcamp.com>
2015-07-15 17:32:48 -0500

    add files we forgot to add before

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
   has_many :questions, foreign_key: :author_id
   has_many :comments, foreign_key: :commenter_id
   has_many :answers, foreign_key: :answerer_id
