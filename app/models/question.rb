6a5e01c37cc20e5064453d19b0d809fba4d7e7ed
Evangeline Garreau & Shawn Wilkinson <pair+egarreau.shawndav@devbootcamp.com>
2015-07-16 10:03:03 -0500

    add option to add a tag and view questions associated with tag

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,13 @@ class Question < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
   has_many :votes, as: :votable
+
+  def tag_list=(tag_list)
+    self.tags = tag_list.split(",").map { |tag| Tag.find_or_initialize_by(name: tag.strip.capitalize)}
+  end
+
+  def tag_list
+    self.tags.map { |tag| tag.name }.join(", ")
+  end
+
 end
